1df1533966e79071bd35a38d6947820a6edef838
Aryo brokolly <98819851+aryobrokolly@users.noreply.github.com>
2024-03-09 00:38:44 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ clear
 
 download_files()
 {
-    	touch $DIR/loghilink.txt
+    	clear
   	echo "Downloading files from repo hilink mak cling..."
    	wget -O $DIR/m https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/m && chmod +x $DIR/m
  	wget -O $DIR/hilink https://raw.githubusercontent.com/aryobrokolly/menghilink/main/usr/bin/hilink && chmod +x $DIR/hilink
